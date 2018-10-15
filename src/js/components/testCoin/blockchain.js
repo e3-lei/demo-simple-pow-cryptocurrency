ef10f812796164893f3e97ca0ce977320cc1870b
e3-lei <e3leihro@gmail.com>
2018-10-15 21:13:13 +0200

    add pow, validChain, hashBLock + test

--- src/js/components/testCoin/blockchain.js
+++ src/js/components/testCoin/blockchain.js
@@ -1,5 +1,8 @@
 export default class Blockchain {
     constructor() {
+        this.hashlib = require('jshashes')
+        this.sha256 = new this.hashlib.SHA256
+
         this.chain = []
         this.transactions = []
         this.nodes = new Set()
@@ -18,5 +21,54 @@ export default class Blockchain {
         this.chain.push(block)
 
         return block
+    }
+
+    getPrevBlock() {
+        return this.chain[-1]
+    }
+
+    pow(prevProof) {
+        let newProof = 1
+        let checkProof = false
+        while (!checkProof) {
+            if (this.isHashValid(this.calcHash(newProof, prevProof))) {
+                return newProof;
+            } 
+
+            newProof++
+        }
+    }
+
+    isChainValid(chain) {
+        let prevBlock = chain[0]
+        let blockIndex = 1
+        while (blockIndex < chain.length) {
+            const block = chain[blockIndex]
+            if (block['previous_hash'] != this.hashBlock(prevBlock)) {
+                return false
+            }
+
+            const hash = this.calcHash(block['proof'], prevBlock['proof'])
+            if (!this.isHashValid(hash)) {
+                return false
+            }
+
+            prevBlock = block
+            blockIndex++
+        }
+
+        return true
+    }
+
+    calcHash(newProof, prevProof) {
+        return this.sha256.hex((Math.pow(newProof, 2) - Math.pow(prevProof, 2)).toString())
+    }
+
+    isHashValid(hash) {
+        return hash.substring(0, 4) === '0000'
+    }
+
+    hashBlock(block) {
+        return this.sha256.hex(JSON.stringify(block, Object.keys(block).sort()));
     }
 }
--- src/js/components/testCoin/blockchain.spec.js
+++ src/js/components/testCoin/blockchain.spec.js
@@ -3,7 +3,7 @@ let assert = require('assert');
 import Blockchain from './blockchain.js';
 
 describe('test for class Blockchain', function() {
-	it('should return insert the first block after init', function() {
+    it('should return insert the first block after init', function() {
         const blockchain = new Blockchain
         const firstBlock = blockchain.chain[0]
 
@@ -13,5 +13,17 @@ describe('test for class Blockchain', function() {
         assert.equal(firstBlock.proof, 1);
         assert.equal(firstBlock.previous_hash, 0);
         assert.equal(firstBlock.transactions.length, 0);
-	});
+    });
+    it('should create new block with pow', () => {
+        const blockchain = new Blockchain
+        const firstBlock = blockchain.chain[0]
+        const secondBlock = blockchain.createBlock(blockchain.pow(firstBlock.proof), blockchain.hashBlock(firstBlock))
+
+        assert.equal(blockchain.chain.length, 2);
+        assert.equal(blockchain.transactions.length, 0);
+        assert.equal(secondBlock.index, 2);
+        assert.equal(secondBlock.previous_hash, blockchain.hashBlock(firstBlock));
+        assert.equal(secondBlock.transactions.length, 0);
+        assert.equal(blockchain.isChainValid(blockchain.chain), true)
+    })
 });
